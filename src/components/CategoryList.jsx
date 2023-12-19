183314f9237327d11493a59673cf950d0eb52cbf
Luen67 <135562795+Luen67@users.noreply.github.com>
2023-12-18 21:32:03 -0600

    Listas Dinamicas

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,17 +1,83 @@
 import CategoryCard from "./CategoryCard"
+
+const categories = [
+    {
+        name: "Charlando",
+        viewers: 508858,
+        genre: "IRL",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/509658-285x380.jpg"
+    },
+    {
+        name: "Anime World",
+        viewers: 5491,
+        genre: "RPG",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/637756067_IGDB-285x380.jpg"
+    },
+    {
+        name: "Fortine",
+        viewers: 231117,
+        genre: "Shooter",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/33214-285x380.jpg"
+    },
+    {
+        name: "The Simpsons: Hit...",
+        viewers: 661,
+        genre: "Plataformas",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/2897_IGDB-285x380.jpg"
+    },
+    {
+        name: "Grand Thef Auto V",
+        viewers: 367646,
+        genre: "Shooter",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-285x380.jpg"
+    },
+    {
+        name: "Eventos especiales",
+        viewers: 8662,
+        genre: "IRL",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/509663-285x380.jpg"
+    },
+    {
+        name: "Dragon Ball",
+        viewers: 1532,
+        genre: "Acción",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/317912951_IGDB-285x380.jpg"
+    },
+    {
+        name: "League of Legends",
+        viewers: 162832,
+        genre: "RPG",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/21779-285x380.jpg"
+    },
+    {
+        name: "Rust",
+        viewers: 185551,
+        genre: "Shooter",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/263490_IGDB-285x380.jpg"
+    },
+    {
+        name: "Dota 2",
+        viewers: 75611,
+        genre: "MOBA",
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/29595-285x380.jpg"
+    }
+]
+
 export default function CategoryList(){
     return(
         <main className="flex flex-wrap gap-3 ms-6">
-            <CategoryCard name="Charlando" viewers={508858} genre="IRL" image="https://static-cdn.jtvnw.net/ttv-boxart/509658-285x380.jpg"/>
-            <CategoryCard name="Anime World" viewers={5491} genre="RPG" image="https://static-cdn.jtvnw.net/ttv-boxart/637756067_IGDB-285x380.jpg"/>
-            <CategoryCard name="Fortine" viewers={231117} genre="Shooter" image="https://static-cdn.jtvnw.net/ttv-boxart/33214-285x380.jpg"/>
-            <CategoryCard name="The Simpsons: Hit..." viewers={661} genre="Plataformas" image="https://static-cdn.jtvnw.net/ttv-boxart/2897_IGDB-285x380.jpg"/>
-            <CategoryCard name="Grand Thef Auto V" viewers={367646} genre="Shooter" image="https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-285x380.jpg"/>
-            <CategoryCard name="Eventos especiales" viewers={8662} genre="IRL" image="https://static-cdn.jtvnw.net/ttv-boxart/509663-285x380.jpg"/>
-            <CategoryCard name="Dragon Ball" viewers={1532} genre="Acción" image="https://static-cdn.jtvnw.net/ttv-boxart/317912951_IGDB-285x380.jpg"/>
-            <CategoryCard name="League of Legends" viewers={162832} genre="RPG" image="https://static-cdn.jtvnw.net/ttv-boxart/21779-285x380.jpg"/>
-            <CategoryCard name="Rust" viewers={185551} genre="Shooter" image="https://static-cdn.jtvnw.net/ttv-boxart/263490_IGDB-285x380.jpg"/>
-            <CategoryCard name="Dota 2" viewers={75611} genre="MOBA" image="https://static-cdn.jtvnw.net/ttv-boxart/29595-285x380.jpg"/>
+            {
+                categories.map((category) =>{
+                    return(
+                    <CategoryCard
+                        name={category.name}
+                        viewers={category.viewers}
+                        genre={category.genre}
+                        image={category.image}
+                    />                       
+                    )
+                })
+            }
         </main>
     )
 }
--- src/components/StreamerList.jsx
+++ src/components/StreamerList.jsx
@@ -1,12 +1,51 @@
 import Streamer from "./Streamer";
+
+const streamers = [
+    {
+        name: "ibai",
+        avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/574228be-01ef-4eab-bc0e-a4f6b68bedba-profile_image-70x70.png",
+        isLive: true,
+        description: "Eventos especi...",
+        viewers: 127890
+    },
+    {
+        name: "easportsfc",
+        avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/d5ac63ef-75f5-4392-9032-ca59a170a4a1-profile_image-70x70.png",
+        isLive: true,
+        description: "EA Sports FC 24",
+        viewers: 211683
+    },
+    {
+        name: "evaanna",
+        avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/ff27b921-6c97-4b5c-ab7e-7a39daad3ea1-profile_image-70x70.png",
+        isLive: true,
+        description: "Piscinas,jacuzzis...",
+        viewers: 2985
+    },
+    {
+        name: "SandrsSkins",
+        avatar: "https://static-cdn.jtvnw.net/jtv_user_pictures/a16b82f7-78c7-4ea9-9203-703f2f50dfcf-profile_image-70x70.png",
+        isLive: true,
+        description: "Charlando",
+        viewers: 1638
+    }
+];
+
 export default function StreamerList(){
     return(
         <aside className="w-[280px] bg-[#1f1f23] h-screen flex flex-col">
             <h2 className="text-[0.9rem] font-semibold uppercase leading-5 p-1">Canales recomendados</h2>
-            <Streamer avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/574228be-01ef-4eab-bc0e-a4f6b68bedba-profile_image-70x70.png" name="ibai" description="Eventos especi..." viewers={127890}/>
-            <Streamer avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/d5ac63ef-75f5-4392-9032-ca59a170a4a1-profile_image-70x70.png" name="easportsfc" description="EA Sports FC 24" viewers={211683}/>
-            <Streamer avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/ff27b921-6c97-4b5c-ab7e-7a39daad3ea1-profile_image-70x70.png" name="evaanna" description="Piscinas,jacuzzis..." viewers={2985}/>
-            <Streamer avatar="https://static-cdn.jtvnw.net/jtv_user_pictures/a16b82f7-78c7-4ea9-9203-703f2f50dfcf-profile_image-70x70.png" name="SandrsSkins" description="Charlando" viewers={1638}/>
+            {
+                streamers.map((streamer) => {
+                    return(
+                        <Streamer 
+                        avatar={streamer.avatar} 
+                        name={streamer.name} 
+                        description={streamer.description} 
+                        viewers={streamer.viewers}/>
+                    )
+                })
+            }
         </aside>
     );
 }
